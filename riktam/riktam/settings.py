1383c74893beb3d647ce17837642629efd3e4437
Sudeep Kumar <sudeepk82@gmail.com>
2023-03-07 02:13:49 +0530

    dotenv in settings

--- riktam/riktam/settings.py
+++ riktam/riktam/settings.py
@@ -11,14 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_doatenv
+from dotenv import load_dotenv
 import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_doatenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
